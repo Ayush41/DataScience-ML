89111c4d03d6d9cf795c922587bdeb5422ac5292
ayush41 <anonymousking41681@gmail.com>
2025-02-13 23:52:22 +0530

    testing api done

--- GROQ/Testingapi.py
+++ GROQ/Testingapi.py
@@ -1,10 +1,18 @@
 import os
-
+from dotenv import load_dotenv
 from groq import Groq
+
+
+# Load environment variables from the .env file
+load_dotenv()
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
+
+# load_dotenv(find_dotenv())
+# genai.configure(api_key=os.getenv("GROQ_API_KEY"))
+# api_key=os.environ.get("GROQ_API_KEY"),
 
 chat_completion = client.chat.completions.create(
     messages=[
